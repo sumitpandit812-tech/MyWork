d89aecceff6615ef4d36cc1685f1fb5caa488d89
sumitpandit812-tech <sumitpandit812@gmail.com>
2025-12-31 22:07:36 +0530

    new file

---
+++ assigment2/Task1.py
@@ -0,0 +1,10 @@
+
+#Task 1: Check if a Number is Even or Odd
+print("""
+""")
+num = int(input("Enter a number:\t "))
+print(""" """)
+if num % 2 == 0:
+          print(f"{num} is an even number")
+else:
+          print(f"{num} is an odd number")
